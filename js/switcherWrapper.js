a51e4f6b7276d410205614388c77385a327b0cfc
Dani <Dani@DESKTOP-NJGRIAT>
2017-01-24 22:02:54 +0100

    Added restyle functionality

--- js/switcherWrapper.js
+++ js/switcherWrapper.js
@@ -7,10 +7,10 @@ define(["jquery","jqueryui", "bootstrap", "openlayers"], function(jquery, jquery
 	self.container;
 	self.treeData;
 
-	self.init = function(container, olWrapper){
+	self.init = function(container, olWrapper, dialogWrapper){
 	    
 	    self.container = container;
-	    
+	    self.dialogWrapper = dialogWrapper;
 	    self.olWrapper = olWrapper;
 	    self.treeData = self.olWrapper.getDataStructureTree();
 	    self.container.html("");
@@ -76,6 +76,74 @@ define(["jquery","jqueryui", "bootstrap", "openlayers"], function(jquery, jquery
 	    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
 	    a.dispatchEvent(e);
  
+	},
+	
+	self.setLayerSettings = function(item){
+
+	    $("#layer_settings #layer_style_fill").spectrum({
+		color: "rgba(255, 141, 71, 0.8)",
+		showAlpha: true,
+		change: function(color) {
+		    $("#layer_settings #layer_style_fill").val(color.toRgbString());
+		}
+	    }).val("rgba(255, 141, 71, 0.8)");
+	    
+	    $("#layer_settings #layer_style_outline").spectrum({
+		color: "rgba(0, 0, 0, 1)",
+		showAlpha: true,
+		change: function(color) {
+		    $("#layer_settings #layer_style_outline").val(color.toRgbString());
+		}
+	    }).val("rgba(0, 0, 0, 1)"); 
+
+	    dialog = $( "#layer_settings" ).dialog({
+	    autoOpen: false,
+	    height: 300,
+	    width: 600,
+	    title: "Layer style",
+	    modal: true,
+	     buttons: {
+		"Restyle" : function(){
+
+		    item.self.getSource().getFeatures().forEach(function(feature, index, array) {
+			if(feature.getGeometry().getType() === "Point") {
+			    feature.setStyle(new ol.style.Style({
+				image: new ol.style.Circle({
+				    radius: 5,
+				    stroke: new ol.style.Stroke({
+					color: $("#layer_settings #layer_style_outline").val(),
+					width: 1
+				    }),
+				    fill: new ol.style.Fill({
+					 color: $("#layer_settings #layer_style_fill").val()
+				    })
+				})
+			    }));
+			} else {
+			    feature.setStyle(new ol.style.Style({
+				stroke: new ol.style.Stroke({
+				    color: $("#layer_settings #layer_style_outline").val(),
+				    width: 1
+				}),
+				fill: new ol.style.Fill({
+				     color: $("#layer_settings #layer_style_fill").val()
+				})
+			    }));
+			}
+		    });
+
+		},
+		Cancel: function() {
+		  dialog.dialog( "close" );
+		}
+	     },
+		close: function() {
+
+		}
+	    });
+	     
+	    dialog.dialog( "open" );
+		
 	},	
 	
 	self.buildTree = function(content = [], current_depth, container){
@@ -91,11 +159,9 @@ define(["jquery","jqueryui", "bootstrap", "openlayers"], function(jquery, jquery
 		    $(delete_icon).addClass("fa fa-trash");
 		    
 		    var visibility_icon = document.createElement('i');
-		  
 
 		    $(visibility_icon).on("click", function(){
 			self.setLayerVisibility(item.self, curr, visibility_icon);
-		
 		    });
 		    
 		    if(item.self.getVisible() === true){
@@ -115,6 +181,14 @@ define(["jquery","jqueryui", "bootstrap", "openlayers"], function(jquery, jquery
 		    } else {
 			$(selectable_icon).addClass("fa fa-check-square-o");
 		    }
+		    
+		    var settings_icon = document.createElement('i');
+		    
+		    $(settings_icon).addClass("fa fa-cog");
+		    
+		    $(settings_icon).on("click", function(){
+			self.setLayerSettings(item);
+		    });
 		    
 		    var download_icon = document.createElement('i');
 		    
@@ -143,10 +217,12 @@ define(["jquery","jqueryui", "bootstrap", "openlayers"], function(jquery, jquery
 			self.buildTree(item.children, current_depth + 1, child_nest);
 
 		    } else if(item.type === "layer"){
-		
-			//delete_icon = '<i class="fa fa-trash" aria-hidden="true">';
+
 			layer_header = '<div class="layer-header"><div class="title">' + item.title + '</div><div class="controls"></div></div>';
 			$(newListItemContainer).addClass("layer").append(layer_header);
+			
+			if(item.self instanceof ol.layer.Vector) $(newListItemContainer).find(".controls").append(settings_icon);
+			
 			$(newListItemContainer).find(".controls").append(selectable_icon);
 			$(newListItemContainer).find(".controls").append(visibility_icon);
 			$(newListItemContainer).find(".controls").append(delete_icon);
@@ -166,8 +242,7 @@ define(["jquery","jqueryui", "bootstrap", "openlayers"], function(jquery, jquery
 			$(container).append(newListItemContainer);
 
 		    }
-		    
-		    
+
 		});
 	};
 
